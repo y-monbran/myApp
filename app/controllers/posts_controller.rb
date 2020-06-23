4a098d748d60511dfdb5e1aecb9f23912325e746
y-monbran <monbran@users.noreply.github.com>
2020-06-23 20:31:21 +0900

    add_implementation 2times

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
-  def index
+  def index #トップページ 
+    @i = "iii"
+  end
+
+  def new #トップページのform_withに繋がる
+    @post = "newページ"
+  end
+
+  private
+  def post_params
+    # params.require(:post).permit(:name, :use_ids [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+  # root to: 'posts#index'
+  get '/', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
 end
